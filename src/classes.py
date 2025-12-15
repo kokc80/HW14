b02d1a1eb37d69ce167955e72b4451f52819c7e2
Kokc80 <kokc@inbox.ru>
2025-12-15 13:40:38 +0900

    Домашка 16.1 исправление ошибок

--- src/classes.py
+++ src/classes.py
@@ -98,10 +98,6 @@ class Smartphone(Product):
 class LawnGrass(Product):
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         super().__init__(name, description, price, quantity)
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
         self.country = country
         self.germination_period = germination_period
         self.color = color
